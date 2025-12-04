c2d75481a109e72cedfd02053946c6a527795c34
patakvolgyivivien <patakvolgyivivien@gmail.com>
2025-12-04 17:20:11 +0100

    some changes

--- tests/int/test_calc_int.py
+++ tests/int/test_calc_int.py
@@ -6,9 +6,9 @@ from calculator import Calculator
 def test_sequence_of_operations_results_and_history():
     calc = Calculator()
 
-    result1 = calc.add(1, 3)         # 4
-    result2 = calc.multiply(result1, 5)  # 15
-    result3 = calc.add(result2, -5)  # 10
+    result1 = calc.add(1, 3)         #4
+    result2 = calc.multiply(result1, 5)  #15
+    result3 = calc.add(result2, -5)  #10
 
     assert result1 == 3
     assert result2 == 15
