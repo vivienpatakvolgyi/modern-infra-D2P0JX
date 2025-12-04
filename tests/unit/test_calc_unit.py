af6ff667a46e7a6cdb52ffdd54ae0da8ffe21bd0
patakvolgyivivien <patakvolgyivivien@gmail.com>
2025-12-04 17:30:20 +0100

    Fix imports and integration test path

--- tests/unit/test_calc_unit.py
+++ tests/unit/test_calc_unit.py
@@ -1,6 +1,13 @@
 import pytest
+import os
+import sys
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
 
 from calculator import Calculator
+
 
 
 @pytest.fixture
